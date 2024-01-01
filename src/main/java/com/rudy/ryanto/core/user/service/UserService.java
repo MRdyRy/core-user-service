8674d7ad2ab5091470654ebcadc3b8f6c16f292d
MRdyRy <ryanserfanru@gmail.com>
2024-01-02 01:53:24 +0700

    update loose coupling

--- src/main/java/com/rudy/ryanto/core/user/service/UserService.java
+++ src/main/java/com/rudy/ryanto/core/user/service/UserService.java
@@ -6,7 +6,6 @@ import com.rudy.ryanto.core.user.exception.CoreUserServiceException;
 import com.rudy.ryanto.core.user.util.UserConstant;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
